adcf3d2d0ed299fdf5bac7698e5885c0225668b4
Dinis Cruz <dinis.cruz@owasp.org>
2025-09-21 12:15:39 +0100

    Initialize MGraph AI Service Cache CLI from template
    - Based on MGraph-AI__Service__Base v0.3.0
    - Service name: mgraph_ai_service_cache__cli
    - Repository: MGraph-AI__Service__Cache__CLI
    - Automated setup complete

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -10,7 +10,7 @@ fi
 NEW_VERSION=$1
 
 # Update version file
-echo -n "$NEW_VERSION" > mgraph_ai_service_base/version
+echo -n "$NEW_VERSION" > mgraph_ai_service_cache__cli/version
 
 # Update pyproject.toml
 if [[ "$OSTYPE" == "darwin"* ]]; then
